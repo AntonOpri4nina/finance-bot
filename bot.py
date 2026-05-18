f11f855671af0b0234b07c77968f6ce3b5e0c601
AntonOpri4nina <suvorovnickol@gmail.com>
2026-05-19 00:14:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.fsm.storage.memory import MemoryStorage  # ✅ aiogram 3.x
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
